37debbcf00f0f9905aac46d546e01ecc45a62256
David Mulyawan <91412794+davdmoo@users.noreply.github.com>
2022-02-24 10:14:14 +0700

    Flutter init and setup product model

---
+++ lib/models/product.dart
@@ -0,0 +1,17 @@
+class Product {
+  final String id;
+  final String name;
+  final String description;
+  final String imageUrl;
+  final double price;
+  bool isFavorite;
+
+  Product({
+    required this.id,
+    required this.name,
+    required this.description,
+    required this.imageUrl,
+    required this.price,
+    this.isFavorite = false,
+  });
+}
