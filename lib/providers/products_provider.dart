5d75cc5ebd1f83b1bca29e3435a6c9ff9f9dbc66
David Mulyawan <91412794+davdmoo@users.noreply.github.com>
2022-02-26 17:40:08 +0700

    Feat: post http request on user product

--- lib/providers/products_provider.dart
+++ lib/providers/products_provider.dart
@@ -1,4 +1,7 @@
+import "dart:convert";
+
 import 'package:flutter/material.dart';
+import "package:http/http.dart" as http;
 
 import "./product.dart";
 
@@ -66,6 +69,18 @@ class Products with ChangeNotifier {
   // }
 
   void addProduct(Product product) {
+    final url = Uri.parse("https://flutter-shop-app-ef36c-default-rtdb.asia-southeast1.firebasedatabase.app/products.json");
+    http.post(
+      url,
+      body: json.encode({
+        "name": product.name,
+        "description": product.description,
+        "imageUrl": product.imageUrl,
+        "price": product.price,
+        "isFavorite": product.isFavorite,
+      }),
+    );
+    
     final newProduct = Product(
       id: DateTime.now().toString(),
       name: product.name,
